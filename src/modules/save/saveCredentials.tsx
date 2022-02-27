ead01b4d1c41c6308eb8a60573221024eed221cf
RonaldColyar <phantexcreations@gmail.com>
2022-02-26 22:13:57 -0600

    advance

--- src/modules/save/saveCredentials.tsx
+++ src/modules/save/saveCredentials.tsx
@@ -5,7 +5,16 @@ interface Nothing {}
 export const SaveCredentials: React.FC<Nothing> = () => {
   const router = useRouter()  
   const {refresh, access} = router.query;
-  typeof window !== 'undefined' ? localStorage.setItem("r-ciot", refresh as string): null
-  typeof window !== 'undefined' ?   localStorage.setItem("a-ciot", access as string): null
+  let issue = false;
+  typeof window !== 'undefined' ? localStorage.setItem("r-ciot", refresh as string): issue = true;
+  typeof window !== 'undefined' ?   localStorage.setItem("a-ciot", access as string): issue = true;
+
+  if (!issue){
+    localStorage.setItem("ciot_auth_status", "good");
+  }
+  else{
+    localStorage.setItem("ciot_auth_status", "bad");
+  }
+
   return(<div></div>)
 }
